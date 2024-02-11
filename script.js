6bc4cabb58a5b9e9801801b7bf9cd55c97b279ae
Fahmi Fisal <ryuzaki_l@y7mail.com>
2024-02-11 22:46:04 +0800

    Update script.js
    change github proxy server

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ const searchButton = document.getElementById("search-button");
 const resultsContainer = document.getElementById("results");
 //corsProxy = "https://cors-anywhere.pulkitpareekofficial.workers.dev/?url=";
 //const corsProxy = "https://just-cros.pages.dev/?url";
-const corsProxy = "https://corsproxy.io/?";
+//const corsProxy = "https://corsproxy.io/?";
+const corsproxy = "https://mirror.ghproxy.com/";
 
 // functions that manipulates the queries of url to get a low resolution image to increase page speed
 function optimisedImageUrl(url) {
