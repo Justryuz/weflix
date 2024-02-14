d9edeca54f7e4bce4fb6947d3e89dd8c6269ec98
Fahmi Fisal <ryuzaki_l@y7mail.com>
2024-02-14 10:23:53 +0800

    Update script.js
    update

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ const scrollToResults = () => {
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const resultsContainer = document.getElementById("results");
-//corsProxy = "https://cors-anywhere.pulkitpareekofficial.workers.dev/?url=";
-//const corsProxy = "https://just-cros.pages.dev/?url";
+corsProxy = "https://cors-anywhere.pulkitpareekofficial.workers.dev/?url=";
+//const corsProxy = "https://just-cros.pages.dev/?url=";
 //const corsProxy = "https://corsproxy.io/?";
-const corsproxy = "https://mirror.ghproxy.com/";
+//const corsproxy = "https://mirror.ghproxy.com/";
 
 // functions that manipulates the queries of url to get a low resolution image to increase page speed
 function optimisedImageUrl(url) {
